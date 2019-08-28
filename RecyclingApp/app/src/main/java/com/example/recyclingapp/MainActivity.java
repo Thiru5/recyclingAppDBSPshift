31499a076707ea8c983bfa25ee371980589299ff
B THIRUKARTHIK <thirukarthik95@gmail.com>
2019-08-28 23:15:57 +0800

    Update - failing camera but camera function exists
    - added camera api
    - added material ui
    - showing jc wu how to do

--- RecyclingApp/app/src/main/java/com/example/recyclingapp/MainActivity.java
+++ RecyclingApp/app/src/main/java/com/example/recyclingapp/MainActivity.java
@@ -3,8 +3,25 @@ package com.example.recyclingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    public void onClickFloatingAction(View buttonView){
+        Intent cameraIntent = new Intent(this, cameraActivity.class);
+        startActivity(cameraIntent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
